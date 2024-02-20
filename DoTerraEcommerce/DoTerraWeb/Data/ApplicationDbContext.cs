cc88f70033bd3fed02be4364df4110af99d2c3cc
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-02-20 18:45:22 -0300

    core: Seed Category Table

--- DoTerraEcommerce/DoTerraWeb/Data/ApplicationDbContext.cs
+++ DoTerraEcommerce/DoTerraWeb/Data/ApplicationDbContext.cs
@@ -10,5 +10,13 @@ namespace DoTerraWeb.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Essential Oils", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "Accessories", DisplayOrder = 2 }
+                );
+        }
     }
 }

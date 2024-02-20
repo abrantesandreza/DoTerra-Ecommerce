0e4c0f557bda6180a3a57d742308b69dc250b483
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-02-20 15:30:14 -0300

    core: Setup ApplicationDbContext

---
+++ DoTerraEcommerce/DoTerraWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DoTerraWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+
+        }
+    }
+}

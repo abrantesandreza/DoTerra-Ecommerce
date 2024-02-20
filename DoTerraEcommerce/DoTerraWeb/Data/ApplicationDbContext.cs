6b4f49cf32c3ea100cfc3ae90b2ad059ae8f6a4f
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-02-20 16:07:41 -0300

    core: Create Database and create Category Table

--- DoTerraEcommerce/DoTerraWeb/Data/ApplicationDbContext.cs
+++ DoTerraEcommerce/DoTerraWeb/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DoTerraWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DoTerraWeb.Data
 {
@@ -6,7 +7,8 @@ namespace DoTerraWeb.Data
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
         {
-
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }

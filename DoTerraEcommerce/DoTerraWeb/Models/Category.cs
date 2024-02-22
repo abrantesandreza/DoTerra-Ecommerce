5742ce5d7638a59d3e9c2a13cb9c4eaad0915526
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-02-21 22:50:59 -0300

    style: Input with Tag helpers

--- DoTerraEcommerce/DoTerraWeb/Models/Category.cs
+++ DoTerraEcommerce/DoTerraWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.Metadata.Ecma335;
 
 namespace DoTerraWeb.Models
@@ -8,7 +9,9 @@ namespace DoTerraWeb.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Nome da categoria")]
         public string Name { get; set; }
+        [DisplayName("Número de identificação")]
         public int DisplayOrder { get; set; }
 
     }

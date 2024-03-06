325add694ecc319e125fc3e10293d1a85cfa4b2e
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-06 17:32:45 -0300

    core: Added and implemented IUnitOfWork in DataAccess

--- DoTerraEcommerce/DoTerra.DataAccess/Repository/UnitOfWork.cs
+++ DoTerraEcommerce/DoTerra.DataAccess/Repository/UnitOfWork.cs
@@ -9,18 +9,19 @@ using System.Threading.Tasks;
 
 namespace DoTerra.DataAccess.Repository
 {
-    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    public class UnitOfWork : IUnitOfWork
     {
         private ApplicationDbContext _db;
-
-        public CategoryRepository(ApplicationDbContext db) : base(db)
+        public ICategoryRepository Category { get;private set; }
+        public UnitOfWork(ApplicationDbContext db)
         {
             _db = db;
+            Category = new CategoryRepository(_db);
         }
 
-        public void Update(Category obj)
+        public void Save()
         {
-            _db.Categories.Update(obj);
+            _db.SaveChanges();
         }
     }
 }

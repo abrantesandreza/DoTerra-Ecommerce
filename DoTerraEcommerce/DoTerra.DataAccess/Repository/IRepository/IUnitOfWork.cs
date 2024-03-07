2559be790f98eb1cf0525f6220984c5bfb591820
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-07 19:22:48 -0300

    core: Added IProductRepository in IUnitOfWork and implemented in UnitOfWork

--- DoTerraEcommerce/DoTerra.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ DoTerraEcommerce/DoTerra.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -9,6 +9,7 @@ namespace DoTerra.DataAccess.Repository.IRepository
     public interface IUnitOfWork
     {
         ICategoryRepository Category { get; }
+        IProductRepository Product { get; }
         void Save();
     }
 }

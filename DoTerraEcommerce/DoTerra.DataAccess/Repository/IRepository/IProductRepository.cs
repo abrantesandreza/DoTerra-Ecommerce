8c590c5fc788b70c45a1f9f494b1c8e51e43e0fe
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-07 18:58:35 -0300

    core: Added and implemented Product Interface

---
+++ DoTerraEcommerce/DoTerra.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using DoTerra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoTerra.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
---
+++ DoTerraEcommerce/DoTerra.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using DoTerra.DataAccess.Data;
+using DoTerra.DataAccess.Repository.IRepository;
+using DoTerra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoTerra.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}

d442752eec74827eecf07036b76039f3152cd994
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-07 23:52:14 -0300

    core: Created ProductController, Index action method and the Index View

---
+++ DoTerraEcommerce/DoTerraWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using DoTerra.DataAccess.Repository.IRepository;
+using DoTerra.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoTerraWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Product> productsList = _unitOfWork.Product.GetAll().ToList();
+            return View(productsList);
+        }
+    }
+}

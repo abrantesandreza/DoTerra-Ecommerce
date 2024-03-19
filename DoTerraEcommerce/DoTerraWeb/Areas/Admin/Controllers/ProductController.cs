ee45f08ff0caf92c24726358b55e734da4bcb939
Andreza Abrantes <andrezaabrantes@gmail.com>
2024-03-19 13:41:02 -0300

    core: Edit method in ProductController implemented, both Get and Post methods and Edit View also implemented

--- DoTerraEcommerce/DoTerraWeb/Areas/Admin/Controllers/ProductController.cs
+++ DoTerraEcommerce/DoTerraWeb/Areas/Admin/Controllers/ProductController.cs
@@ -58,5 +58,35 @@ namespace DoTerraWeb.Areas.Admin.Controllers
 
             return View(productFromDb);
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(productFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
